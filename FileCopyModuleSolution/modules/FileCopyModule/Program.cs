0fdf14eb899ed6b364814f5fa3ba1fa04f05319c
Alpa Kohli <alkohli@microsoft.com>
2018-10-18 14:54:58 -0700

    Added filecopymodule solution

---
+++ FileCopyModuleSolution/modules/FileCopyModule/Program.cs
@@ -0,0 +1,128 @@
+namespace FileCopyModule
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static int counter;
+        private const string InputFolderPath = "/home/LocalShare";
+        private const string OutputFolderPath = "/home/CloudShare";
+
+       /// <summary>
+       /// The MessageBody class defines the expected schema for the body of incoming messages. 
+       /// </summary>
+        private class FileEvent
+        {
+            public string ChangeType { get; set; }
+
+            public string ShareRelativeFilePath { get; set; }
+
+            public string ShareName { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Init().Wait();
+
+            // Wait until the app unloads or is canceled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when the app is canceled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient and sets up the callback to receive
+        /// messages containing file event information
+        /// </summary>
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the IoT Edge runtime
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            // Register callback to be called when a message is received by the module
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", FileCopy, ioTHubModuleClient);
+        }
+
+        /// <summary>
+        /// This method is called whenever the module is sent a message from the IoT Edge Hub. 
+        /// This method deserializes the file event, extracts the corresponding relative file path, and creates the aboslute input file path using the relative file path and the InputFolderPath.
+        /// This method also forms the absolute output file path using the relative file path and the OutputFolderPath. It then copies the input file to output file and deletes the input file after the copy is complete.
+        /// </summary>
+        static async Task<MessageResponse> FileCopy(Message message, object userContext)
+        {
+            int counterValue = Interlocked.Increment(ref counter);
+
+            try
+            {
+                byte[] messageBytes = message.GetBytes();
+                string messageString = Encoding.UTF8.GetString(messageBytes);
+                Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+                if (!string.IsNullOrEmpty(messageString))
+                {
+                    var fileEvent = JsonConvert.DeserializeObject<FileEvent>(messageString);
+
+                    string relativeFileName = fileEvent.ShareRelativeFilePath.Replace("\\", "/");
+                    string inputFilePath = InputFolderPath + relativeFileName;
+                    string outputFilePath = OutputFolderPath + relativeFileName;
+
+                
+
+                    if (File.Exists(inputFilePath))                
+                    {
+                        Console.WriteLine($"Copying input file: {inputFilePath} to output file: {outputFilePath}");
+                        var outputDir = Path.GetDirectoryName(outputFilePath);
+                        if (!Directory.Exists(outputDir))
+                        {
+                            Directory.CreateDirectory(outputDir);
+                        }
+
+                        File.Copy(inputFilePath, outputFilePath, true);
+                        Console.WriteLine($"Copied input file: {inputFilePath} to output file: {outputFilePath}");
+                        File.Delete(inputFilePath);
+                        Console.WriteLine($"Deleted input file: {inputFilePath}");
+                    } 
+                    else
+                    {
+                        Console.WriteLine($"Skipping this event as input file doesn't exist: {inputFilePath}");   
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught exception: {0}", ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.WriteLine($"Processed event.");
+            return MessageResponse.Completed;
+        }
+
+    }
+}
